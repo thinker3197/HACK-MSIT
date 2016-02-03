5012793ae6e4797e18b9b5c391d89b57cd0f6de4
ayush <ayushkumarsingh97@gmail.com>
2016-02-03 09:47:26 +0530

    backend

---
+++ static/avatar.js
@@ -0,0 +1,13 @@
+var col = 'rgb(' + Math.floor(Math.random()*256)+ ',' +Math.floor(Math.random()*256) +','+Math.floor(Math.random()*256) +')';
+
+$(document).ready(function()
+	{
+
+		$("#avatar").css("background-color",col);
+
+	});
+
+
+
+
+
